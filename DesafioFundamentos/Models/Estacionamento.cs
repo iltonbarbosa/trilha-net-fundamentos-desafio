3a4d4b4c62086037a19c0b2abb936dd3b466335a
Ilton Barbosa <jiltonbarbosa@gmail.com>
2023-07-14 09:24:15 -0300

    desafio 1

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -17,6 +17,7 @@ namespace DesafioFundamentos.Models
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+			veiculos.Add(Console.ReadLine());
         }
 
         public void RemoverVeiculo()
@@ -26,6 +27,7 @@ namespace DesafioFundamentos.Models
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
             string placa = "";
+			placa = Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
@@ -38,8 +40,14 @@ namespace DesafioFundamentos.Models
                 int horas = 0;
                 decimal valorTotal = 0; 
 
+				 Console.WriteLine("Informe o total de horas que o veículo ficou estacionado:");
+				 horas = Convert.ToInt16(Console.ReadLine());
+				 valorTotal = precoInicial + precoPorHora * horas;
+
+
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
+				veiculos.Remove(placa);
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
@@ -57,6 +65,9 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
+				for(int x = 0; x < veiculos.Count; x++){
+					Console.WriteLine("Veículo "+x+" : "+veiculos[x]);
+				}
             }
             else
             {
